a65d703339a1318ca4e00bd9a77ae54c365a4fb5
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-10-31 23:06:14 +0530

    app connected to mongoDB

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path : "./.env"
+  path: "./.env"
 });
 
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App is listening on port http://localhost:${port}`)
+    })
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+    process.exit(1);
+  })
 
