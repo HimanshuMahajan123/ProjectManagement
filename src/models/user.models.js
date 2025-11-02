d0326dd2af1ceccb8943379e8d9127973522a621
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-11-02 23:24:26 +0530

    add jsonwebtoken for JWT handling and enhance user model with token generation methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,7 @@
 import mongoose , {Schema} from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 
 const userSchema = new Schema(
@@ -72,7 +74,7 @@ const userSchema = new Schema(
     }
 )
 
-// Hash password before saving the user model
+// Hash password before saving the user model(pre-save hook of mongoose)
 userSchema.pre("save" , async function(next){
     if(!this.isModified("password")){
         return next();
@@ -81,5 +83,46 @@ userSchema.pre("save" , async function(next){
     this.password = await bcrypt.hash(this.password , 10);
     next();
 })
+
+//mongoose method to compare passwords
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password , this.password);
+}
+
+
+//mongoose method to generate JWTs
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            username : this.username,
+            email : this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn : process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn : process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+//mongoose method to generate temporary tokens(for email verification , password reset etc) using crypto(a nodejs built-in module)
+userSchema.methods.generateTemporaryToken = function(){
+    const unhashedToken = crypto.randomBytes(20).toString("hex");
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unhashedToken)
+        .digest("hex");
+    const tokenExpiry = Date.now() + (10*60*1000); //20 minutes from now
+    return { unhashedToken , hashedToken , tokenExpiry };
+}
+
 
 export const User = mongoose.model("User" , userSchema);
